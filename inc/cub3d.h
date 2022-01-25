c79f072449126af2bc10f7d7f556a6d0a6ff111b
Jacopo Martini <jacopomartini89@gmail.com>
2022-01-25 19:58:43 +0100

    makefile libs structure

---
+++ inc/cub3d.h
@@ -0,0 +1,11 @@
+#ifndef CUB3D_H
+# define CUB3D_H
+
+/* SYS LIBRARIES */
+# include <stdio.h>
+
+/* LOCAL LIBRARIES*/
+// # include "mlx.h"
+# include "libft.h"
+
+#endif
---
+++ lib/lib.mk
@@ -0,0 +1,17 @@
+# all : libft.a libmlx.dylib
+
+libft.a :
+	@make -s -C ./libft
+	@cp ./libft/libft.a .
+	@make fclean -C ./libft
+	@echo "\033[32m"$@" compilation successfull\033[0m"
+
+libmlx.dylib :
+	@make -s -C ./mlx
+	@cp ./mlx/$@ .
+	@make fclean -C ./mlx
+	@echo "\033[32m"$@" compilation successfull\033[0m"
+
+clean :
+	@rm -f libft.a libftprintf.a
+	@echo "\033[33mlibraries clean completed\033[0m"
