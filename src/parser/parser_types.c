61cb0c31b2c5a073640a37a8ca2e0e54b746dae1
marcello <marcello.crisari96@gmail.com>
2022-01-28 17:40:18 +0100

    wip parsing_types

--- src/parser/parser_types.c
+++ src/parser/parser_types.c
@@ -27,6 +27,25 @@ static int
 		i++;
 	return i;
 }
+void
+	ft_is_valid_path(char *split)
+{
+	int i;
+
+	i = open(split, O_RDONLY);
+	if (i == -1)
+		ft_fail(ERR_ARGS);
+	close(i);
+	return;
+	
+}
+
+void
+	ft_is_valid_rgb(char * split)
+{
+	(void)split;
+}
+
 
 void
 	ft_types(int fd, t_map *map)
@@ -34,9 +53,9 @@ void
 	int		ret;
 	char	*line;
 	char	**split;
+	int		test;
 
-	int test = 0;
-
+	test = 0;
 	ft_types_init(map);
 	ret = 1;
 	while (ret > 0)
@@ -46,17 +65,29 @@ void
 		if (!split)
 			ft_fail(ERR_SYS_MALLOC);
 		if (!(ft_split_cnt(split) % 2)
-			|| (ft_split_cnt(split) == 1 && !ft_strlen(split[0])))
+			|| (!ft_strlen(split[0])))
 		{
-			test++;
-			if (split[0]) printf("%s\n", split[0]);
-			else printf("CIAO\n");
-			if (test == 3) return;
-		//		IF (LINE != VALID) FAIL
-		//		PARSE
-		//		IF (TYPES == END) RETURN
+			if(ft_split_cnt(split))
+			{
+				test++;
+				if (!ft_memcmp(split[0], "NO", strlen(split[0]))
+					|| !ft_memcmp(split[0], "SO", strlen(split[0]))
+					|| !ft_memcmp(split[0], "WE", strlen(split[0]))
+					|| !ft_memcmp(split[0], "EA", strlen(split[0])))
+					ft_is_valid_path(split[1]);
+				else if (!ft_memcmp(split[0], "F", strlen(split[0]))
+					|| !ft_memcmp(split[0], "C", strlen(split[0])))
+					ft_is_valid_rgb(split[1]);
+				else
+					ft_fail(ERR_ARGS);
+			//		PARSE
+			//		IF (TYPES == END) RETURN
+			if (test == 4)
+				return;
+
+			}
 		}
-		ft_fail(ERR_GENERIC);
 	}
+		ft_fail(ERR_GENERIC);
 	//	FAIL
 }
